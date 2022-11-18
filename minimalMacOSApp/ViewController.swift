d64ccb9e3ed0896d5d32fb7c87cb099e49003718
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2022-11-18 22:02:16 +0800

    Initial Commit

---
+++ minimalMacOSApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  minimalMacOSApp
+//
+//  Created by Kai Quan Tay on 18/11/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
