3ec1f6e2a1837b36d1cdea628637f2320ba43a89
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2022-11-29 08:58:12 +0800

    Add default implementations of functions

--- Sources/macAppBoilerplate/Interface/TabBarProtocol.swift
+++ Sources/macAppBoilerplate/Interface/TabBarProtocol.swift
@@ -8,36 +8,40 @@
 import SwiftUI
 
 public protocol TabBarProtocol {
-    func minimumTabWidth() -> CGFloat?
-    func tabBecomesSmall() -> CGFloat?
-    func maximumTabWidth() -> CGFloat?
-    func animationDuration() -> CGFloat?
-    func tabBarViewHeight() -> CGFloat?
+    func getMinimumTabWidth() -> CGFloat?
+    func getTabBecomesSmall() -> CGFloat?
+    func getMaximumTabWidth() -> CGFloat?
+    func getAnimationDuration() -> CGFloat?
+    func getTabBarViewHeight() -> CGFloat?
+    func getDisableTabs() -> Bool?
+
     func configWindow(_ window: NSWindow) -> Void
-    func disableTabs() -> Bool?
 }
 
 extension TabBarProtocol {
-    var minimumTabWidth: CGFloat { minimumTabWidth() ?? 60 }
-    var tabBecomesSmall: CGFloat { tabBecomesSmall() ?? 60 }
-    var maximumTabWidth: CGFloat { maximumTabWidth() ?? 120 }
-    var animationDuration: CGFloat { animationDuration() ?? 0.3 }
-    var tabBarViewHeight: CGFloat { tabBarViewHeight() ?? 28 }
-    var disableTabs: Bool { disableTabs() ?? false }
-}
+    // default implementations
+    func getMinimumTabWidth() -> CGFloat? { nil }
+    func getTabBecomesSmall() -> CGFloat? { nil }
+    func getMaximumTabWidth() -> CGFloat? { nil }
+    func getAnimationDuration() -> CGFloat? { nil }
+    func getTabBarViewHeight() -> CGFloat? { nil }
+    func getDisableTabs() -> Bool? { nil }
 
-class DefaultTabBarProtocol: TabBarProtocol {
-    func minimumTabWidth() -> CGFloat? { 60 }
-    func tabBecomesSmall() -> CGFloat? { 60 }
-    func maximumTabWidth() -> CGFloat? { 120 }
-    func animationDuration() -> CGFloat? { 0.3 }
-    func tabBarViewHeight() -> CGFloat? { 28 }
     func configWindow(_ window: NSWindow) {
         window.titleVisibility = .hidden
         window.toolbarStyle = .unifiedCompact
         window.titlebarAppearsTransparent = false
         window.titlebarSeparatorStyle = .automatic
     }
-    func toolbarStyle() -> NSWindow.ToolbarStyle? { .unifiedCompact }
-    func disableTabs() -> Bool? { false }
+
+    // safe access variables
+    var minimumTabWidth: CGFloat { getMinimumTabWidth() ?? 60 }
+    var tabBecomesSmall: CGFloat { getTabBecomesSmall() ?? 60 }
+    var maximumTabWidth: CGFloat { getMaximumTabWidth() ?? 120 }
+    var animationDuration: CGFloat { getAnimationDuration() ?? 0.3 }
+    var tabBarViewHeight: CGFloat { getTabBarViewHeight() ?? 28 }
+    var disableTabs: Bool { getDisableTabs() ?? false }
+}
+
+class DefaultTabBarProtocol: TabBarProtocol {
 }
